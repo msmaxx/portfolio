f6411044efa3f20b036a3a5effe34a269b8910c8
Maksim <42644986+msmaxx@users.noreply.github.com>
2021-12-13 21:18:21 +0200

    Update learningExperience.js

--- components/learningExperience.js
+++ components/learningExperience.js
@@ -12,7 +12,7 @@ export default function LearningExperience() {
                                     <li className="underline">Aljona Surzhikova</li>
                                 </li>
                                 <div className="my-2">
-                                    "Robotiteatri projekt koosnes kolmest grupist: robotite programmeerijad, turundus ja
+                                    &quot;Robotiteatri projekt koosnes kolmest grupist: robotite programmeerijad, turundus ja
                                     meie grupp oli dokumentaalfilmi grupp. Meie pidime jälgima robotiteatri loomist,
                                     filmima making-of stseene, looma väikesed klipid, mida turundusgrupp saaks kasutada.
                                     Mina olin grupijuht, kuna olen dokumentaalfilmide režissöör. Me arutlesime filmi
@@ -26,7 +26,7 @@ export default function LearningExperience() {
                                     tegime lavastatud osa inimese ja roboti koostööst. Ergo oli rohkem toimetaja rollis.
                                     Maksim tegi subtiitrid ja monteeris klipid, mida Triin filmis.
                                     Kindlasti oli meie rühma dünaamika kasvav. Nüüd me teame paremini, kes mida saab
-                                    teha."
+                                    teha.&ldquo;
 
                                 </div>
 
@@ -34,21 +34,21 @@ export default function LearningExperience() {
                                     <li className="underline">Egon Gentalen</li>
                                 </li>
                                 <div className="my-2">
-                                    "Liitusin selle projektiga, kuna leidsin teatri ja robotite teema sidumise
+                                    &quot;Liitusin selle projektiga, kuna leidsin teatri ja robotite teema sidumise
                                     huvitavana. Tegelenud ise teatriga, ootasingi näha ja mõista robotite tegevust laval
                                     ja ruumis. Õppisin projektis ajaplaneerimist ning pinge all töötamist. Üks peamisi
                                     raskusi oli kommunikatsioonis mis tulenes aeg-ajalt. Siis sain aru et peab pidevalt
                                     olema aktiivne ja tegema häält. Tulevikus oleksin veelgi rohkem aktiivsem. Projekti
                                     lõpetades tunnen et sain mingisuguse arengu osaliseks. Valgustasin end roboti ja
                                     teatri teemas mis oli väga huvitav ja algusest peale üks eesmärke. Siit võtan kaasa
-                                    oskuse pühenduda ja lükata ennast veelgi rohkem edasi."
+                                    oskuse pühenduda ja lükata ennast veelgi rohkem edasi.&ldquo;
                                 </div>
 
                                 <li>
                                     <li className="underline">Maksim Sitkin</li>
                                 </li>
                                 <div className="my-2">
-                                    "Kõige tähtsam, mis ma projektist sain, oli meeskonnatöö kogemus, oskus tööd jagada
+                                    &quot;Kõige tähtsam, mis ma projektist sain, oli meeskonnatöö kogemus, oskus tööd jagada
                                     ja suhelda teiste meeskonnaliikmetega. Meeskonnas töötades sain uusi teadmisi
                                     videoklippide monteerimisest, kasutades mitmeid erinevaid programme, mis oli väga
                                     hariv ja huvitav. Õppisin, kuidas luua videoklippide sünkroontõlget ning kuidas
@@ -57,7 +57,7 @@ export default function LearningExperience() {
                                     kuid tegelikult läks hästi ja sain positiivset tagasisidet, mis motiveeris mind
                                     edasi töötama.Tulevikus tahaksin töötada meeskonnas, kus on minu erialaga lähedalt
                                     seotud inimesed, sest meil oleks rohkem ühiseid huvisid, sest siis saaksin palju
-                                    rohkem panustada projekti arendamisse."
+                                    rohkem panustada projekti arendamisse.&ldquo;
                                 </div>
 
                                 <li>
@@ -71,7 +71,7 @@ export default function LearningExperience() {
                                     <li className="underline">Marek Nurmbek</li>
                                 </li>
                                 <div className="my-2">
-                                    "Mina kuulusin robotiteatri dokumentaali rühma. Minu ülesanded olid grupis; võtetel
+                                    &quot;Mina kuulusin robotiteatri dokumentaali rühma. Minu ülesanded olid grupis; võtetel
                                     osalemine, videoklippides mängimine, grupiga aruteludes osalemine ja näitlejatele
                                     kirjutamine intervjuude jaoks.
                                     Grupidünaamika sujus üldiselt hästi, grupis oli 2 väga motiveeritud inimest, kes
@@ -82,7 +82,7 @@ export default function LearningExperience() {
                                     üldiselt sujus grupidünaamika hästi.
                                     Leian, et oleksin võinud natukene rohkem panustada. Võib-olla mingites
                                     situatsioonides jäi initsiatiiv näitamisest puudu. Kõik ülesanded, mis mulle anti
-                                    tegin ära, aga leian, et oleksin võinud veel panustada."
+                                    tegin ära, aga leian, et oleksin võinud veel panustada.&ldquo;
                                 </div>
                             </ul>
                         </p>
