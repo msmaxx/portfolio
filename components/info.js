9f3faf8077a3cb9a54485d0d4f39225c80cc6a2b
Maksim <42644986+msmaxx@users.noreply.github.com>
2021-12-13 03:53:39 +0200

    Update info.js

--- components/info.js
+++ components/info.js
@@ -5,6 +5,21 @@ export default function Info() {
                 <div className="items-center lg:flex">
                     <div className="lg:w-full">
                         <h2 className="text-xl font-bold text-gray-800 dark:text-gray-100">Projekti aruanne</h2>
+
+                        <div id="main" className="grid  grid-flow-col my-4">
+                            <div className="rounded-lg">
+                                <img className="object-cover object-center w-60 rounded-md shadow"
+                                     src="https://www.tlu.ee/sites/default/files/TUKO/%C3%9Clikoolo%20logo/TLU-logo-pilt-vrv-suur.jpg"
+                                />
+                            </div>
+                            <div className="rounded-lg">
+                                <img className="object-cover object-center w-60 rounded-md shadow"
+                                     src="https://www.tlu.ee/sites/default/files/TUKO/%C3%9Clikoolo%20logo/TLU-logo-pilt-vrv-suur.jpg"
+                                />
+                            </div>
+                            <div className="bg-green-500 rounded-lg m-4 h-12">3</div>
+
+                        </div>
                     </div>
                 </div>
             </div>
